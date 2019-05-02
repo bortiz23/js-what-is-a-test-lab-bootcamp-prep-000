abd6cde5ba21ebb6e167f5f7c6e28cd72da5defe
Brandon Ortiz <bortiz328@gmail.com>
2019-05-02 15:22:42 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,7 @@
 var index = require("../index.js")
 
-describe('what-is-a-test', (40
+describe('what-is-a-test', (40) =>{
+  describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
