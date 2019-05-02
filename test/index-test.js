7b87e3ef595946447d397623b036b075c5032564
Brandon Ortiz <bortiz328@gmail.com>
2019-05-02 15:25:08 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -3,14 +3,14 @@ var index = require("../index.js")
 describe('what-is-a-test', () =>{
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(index.name).toEqual('Susan')
+      expect("74").toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
     it('is less than 40', () => {
-      expect(74).toBeLessThan(40)
+      expect("74").toBeLessThan("40")
     })
   })
 
